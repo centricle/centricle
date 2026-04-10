3689c34c3e0f7aa324de0aab32b4d0b95bc68018
Kevin Smith <ksmith@centricle.com>
2026-04-09 23:58:10 -0400

    Remove thoughts microblog from centricle.com

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,3 @@
 import { defineCollection, z } from 'astro:content';
 
-const thoughts = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-  }),
-});
-
-export const collections = { thoughts };
+export const collections = {};
